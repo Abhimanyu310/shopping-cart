cbe3f71e9ee63652c3b16e69d341110ca2d86c5c
Abhimanyu310 <abhip310@gmail.com>
2016-07-29 19:50:35 +0530

    Initial commit. Created front end view, dummy data into db and display data in front end

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var Product = require('../models/product');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    Product.find(function (err, doc) {
+        res.render('shop/index', { title: 'Express', products: doc });
+    });
+});
+
+module.exports = router;
