dd94ce5af7e46c5359d4a9d74805f3c8aefb132a
Abhimanyu310 <abhip310@gmail.com>
2016-07-31 18:41:51 +0530

    Added session store to mongo using connect-mongo. Created card model, and can add items to cart properly

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 var Product = require('../models/product');
+var Cart = require('../models/cart');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Product.find(function (err, doc) {
         res.render('shop/index', { title: 'Express', products: doc });
+    });
+});
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+    Product.findById(productId, function (err, product) {
+        if (err){
+           return res.redirect('/');
+        }
+        cart.add(product, productId);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
     });
 });
 
