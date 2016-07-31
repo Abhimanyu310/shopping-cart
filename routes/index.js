456012270574908bada12f4ed93103b2a750a1e5
Abhimanyu310 <abhip310@gmail.com>
2016-07-31 20:25:34 +0530

    Implemented payments using Stripe with complete verification

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,10 @@ var Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var successMsg = req.flash('success')[0];
     Product.find(function (err, doc) {
-        res.render('shop/index', { title: 'Express', products: doc });
+        res.render('shop/index', { title: 'Express', products: doc,
+        successMsg: successMsg, noMessages: !successMsg});
     });
 });
 
@@ -34,5 +36,41 @@ router.get('/shopping-cart', function(req, res, next) {
     res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
+
+router.get('/checkout', function(req, res, next) {
+    if (!req.session.cart){
+        return res.render('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    var errMsg = req.flash('error')[0];
+    res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', function(req, res, next) {
+    if (!req.session.cart){
+        return res.render('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+
+    var stripe = require("stripe")(
+        "sk_test_TkEXqKE7JN45CHHq9rDmPmnv"
+    );
+
+    stripe.charges.create({
+        amount: cart.totalPrice * 100,
+        currency: "usd",
+        source: req.body.stripeToken, // obtained with Stripe.js
+        description: "Test charge"
+    }, function(err, charge) {
+        if (err){
+            req.flash('error', err.message);
+            return res.redirect('/checkout');
+        }
+        req.flash('success', 'Successfully bought');
+        req.session.cart = null
+        return res.redirect('/');
+
+    });
+});
 
 module.exports = router;
